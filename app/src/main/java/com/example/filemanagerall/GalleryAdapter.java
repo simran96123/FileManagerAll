de353d50ec75e94591b93866cee7432ffa337713
DELL-PC <misssimrankumari98@gmail.com>
2020-08-10 00:54:47 +0530

    changes

--- app/src/main/java/com/example/filemanagerall/GalleryAdapter.java
+++ app/src/main/java/com/example/filemanagerall/GalleryAdapter.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class GalleryAdapter  extends RecyclerView.Adapter<GalleryAdapter.ViewHolder>{
+    // for videos
     private Context context;
     private List<String> images;
     private  photoListener  photoListener;
--- app/src/main/java/com/example/filemanagerall/MainActivity.java
+++ app/src/main/java/com/example/filemanagerall/MainActivity.java
@@ -10,18 +10,32 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button imagebtn, audio, video, document;
+    //For videos
+    AdapterVideoFolder obj_adapter;
+    ArrayList<Model_Video> al_video = new ArrayList<>();
+    RecyclerView.LayoutManager recyclerViewLayoutManager_Video;
+    RecyclerView recyclerView_Video;
+   // VideoView vvVideo;
+
+
+    Button imagebtn ,  videobtn;
     RecyclerView recyclerView;
     GalleryAdapter galleryAdapter;
     List<String> images;
@@ -36,11 +50,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       // init();
+
         setContentView(R.layout.activity_main);
         gallery_number = findViewById(R.id.galleryNumber);
-        recyclerView = findViewById(R.id.recyclerView);//        image = findViewById(R.id.Image);
+
+        recyclerView = findViewById(R.id.recyclerView);//
         imagebtn = findViewById(R.id.imageButton);
-//        video = findViewById(R.id.Video);
+        videobtn = findViewById(R.id.videoButton);
 //        document = findViewById(R.id.Document);
 
 
@@ -58,9 +75,78 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            videobtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    init();
+                   // fn_video();
+                }
+            });
+
+
+
+
         }
 
     }
+
+    private void init(){
+
+        recyclerView_Video= (RecyclerView) findViewById(R.id.recyclerViewVideo);
+        recyclerViewLayoutManager_Video = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView_Video.setLayoutManager(recyclerViewLayoutManager_Video);
+        fn_video();
+
+
+
+    }
+
+
+
+    public void fn_video() {
+
+        int int_position = 0;
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name,column_id,thum;
+
+        String absolutePathOfImage = null;
+        uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Video.Media.BUCKET_DISPLAY_NAME,MediaStore.Video.Media._ID,MediaStore.Video.Thumbnails.DATA};
+
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+        cursor = getApplicationContext().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
+        column_id = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+        thum = cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
+
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            Log.e("Column", absolutePathOfImage);
+            Log.e("Folder", cursor.getString(column_index_folder_name));
+            Log.e("column_id", cursor.getString(column_id));
+            Log.e("thum", cursor.getString(thum));
+
+            Model_Video obj_model = new Model_Video();
+            obj_model.setBoolean_selected(false);
+            obj_model.setStr_path(absolutePathOfImage);
+            obj_model.setStr_thumb(cursor.getString(thum));
+
+            al_video.add(obj_model);
+
+        }
+
+        obj_adapter = new AdapterVideoFolder(getApplicationContext(),al_video,MainActivity.this);
+        recyclerView_Video.setAdapter(obj_adapter);
+
+
+    }
+
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void loadImages() {
@@ -90,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
                 loadImages();
+                fn_video();
             }
             else {
 
